779059f7eac2535067f8d92b0886afea31c1bf03
Tom Green <kingofcuddles@gmail.com>
2013-03-13 23:20:03 +0000

    Changed local environment to dev. Removed hard coded url in start.php in favour of *.co.uk & *.com. .gitignored environment files.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -25,8 +25,8 @@ $app = new Illuminate\Foundation\Application;
 */
 
 $env = $app->detectEnvironment(array(
-	'local' => array('*.dev'),
-	'live' => array('zomfshark.com'),
+	'dev' => array('*.dev'),
+	'live' => array('*.com', '.co.uk'),
 ));
 
 /*
