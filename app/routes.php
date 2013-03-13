d132318898bb51647902511ab5ea3a697aae5d0f
Tom Green <kingofcuddles@gmail.com>
2013-03-13 23:06:58 +0000

    Cleaned up routes.

--- app/routes.php
+++ app/routes.php
@@ -1,30 +1,25 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
 Route::get('/', function()
 {
 	return View::make('hello');
 });
 
+
+//API Routes
 Route::group(array('prefix' => 'api/v1'), function()
 {
-	//Skill Items Json
-	Route::get('json/skills', 'ApiJsonSkillController@index');
-	Route::get('json/skills/{skill}', 'ApiJsonSkillController@show');
+	//XML Routes
+	Route::group(array('prefix' => 'xml'), function()
+	{
+		Route::resource('skills', 'ApiXmlSkillController');
 
-	//Skill Items XML
-	Route::get('xml/skills', 'ApiXmlSkillController@index');
-	Route::get('xml/skills/{skill}', 'ApiXmlSkillController@show');
+	});
+
+	//Json Routes
+	Route::group(array('prefix' => 'json'), function()
+	{
+		Route::resource('skills', 'ApiJsonSkillController');
+
+	});
 });
-
-Route::resource('skill', 'SkillController');
