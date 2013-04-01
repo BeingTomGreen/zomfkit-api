f9dad7a0040a7ed4fabb532892aac9d8d223c98f
Tom Green <kingofcuddles@gmail.com>
2013-04-01 12:31:59 +0100

    Switched spaces to tabs...

--- app/controllers/SkillController.php
+++ app/controllers/SkillController.php
@@ -2,29 +2,29 @@
 
 class SkillController extends BaseController {
 
-  public function index()
-  {
-    //Grab all of the Skill Items
-    $skillItems = Skill::all();
+	public function index()
+	{
+		//Grab all of the Skill Items
+		$skillItems = Skill::all();
 
-    //Return the Skill Items as a Json Response
-    return Response::json(array(
-        'items' => $skillItems->toArray()
-      ),
-      201
-    );
-  }
+		//Return the Skill Items as a Json Response
+		return Response::json(array(
+				'items' => $skillItems->toArray()
+			),
+			201
+		);
+	}
 
-  public function show($skill)
-  {
-    //Grab the Skill Items where Skill = $skill
-    $skillItems = Skill::where('item_skill', $skill)->get();
+	public function show($skill)
+	{
+		//Grab the Skill Items where Skill = $skill
+		$skillItems = Skill::where('item_skill', $skill)->get();
 
-    //Return the Skill Items as a Json Response
-    return Response::json(array(
-        'items' => $skillItems->toArray()
-      ),
-      200
-    );
-  }
+		//Return the Skill Items as a Json Response
+		return Response::json(array(
+				'items' => $skillItems->toArray()
+			),
+			200
+		);
+	}
 }
