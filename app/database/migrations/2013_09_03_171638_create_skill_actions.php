58f619a47cd3acb7ee84feeb3d569f99ac058cff
Tom Green <kingofcuddles@gmail.com>
2013-09-03 18:29:01 +0100

    Added create skill actions migration.
    Signed-off-by: Tom Green <kingofcuddles@gmail.com>

---
+++ app/database/migrations/2013_09_03_171638_create_skill_actions.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSkillItems extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('skill_actions', function($table) {
+			$table->increments('action_id');
+			$table->string('action_skill');
+			$table->string('action_name');
+			$table->string('action_level');
+			$table->string('action_experience');
+			$table->string('action_members');
+			$table->string('action_category');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('skill_actions');
+	}
+
+}
